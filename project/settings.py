f28831bb6e401e532ee73304992d9a19c4231f84
Jadav Shyam <shyamjadav628@gmail.com>
2026-06-06 22:07:36 +0530

    Add STATICFILES_DIRS to settings

--- project/settings.py
+++ project/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATICFILES_DIRS = [BASE_DIR / 'builder' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
